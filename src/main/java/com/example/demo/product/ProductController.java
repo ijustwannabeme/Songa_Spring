5b9f2bba2208fe9d13f2c84c01d451854629bb22
liarcrown <liarcrown@naver.com>
2024-08-13 17:22:36 +0900

    feat: songa first

---
+++ src/main/java/com/example/demo/product/ProductController.java
@@ -0,0 +1,12 @@
+package com.example.demo.product;
+
+import org.springframework.web.bind.annotation.RestController;
+
+// 나, 컨트롤러야~
+@RestController
+public class ProductController {
+
+    //"사용자 요청을 던지면"
+    //그에 맞는 로직을 수행할수 있도록 서비스한테 시킨다.
+
+}
